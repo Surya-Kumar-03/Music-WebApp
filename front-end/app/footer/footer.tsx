62aab789c5e3625659f3b18ffe64e82c9be33786
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-04-22 05:14:21 +0530

    login and navbar done

---
+++ front-end/app/footer/footer.tsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+	return (
+		<>
+			<div className='flex justify-center items-center bg-slate-100 h-full'>
+				Create By
+				<a
+					href='https://www.linkedin.com/in/aryan-amish'
+					className='px-1 hover:underline font-semibold'
+				>
+					Aryan
+				</a>
+				&
+				<a
+					href='https://www.linkedin.com/in/bandepalli-surya/'
+					className='px-1 hover:underline font-semibold'
+				>
+					Surya
+				</a>
+			</div>
+		</>
+	);
+};
+
+export default Footer;
--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import LoginForm from "./loginForm";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const Home = () => {
 	return (
-		<div>
-			<LoginForm />
-		</div>
+		<>
+			<div></div>
+		</>
 	);
-}
+};
+
+export default Home;
